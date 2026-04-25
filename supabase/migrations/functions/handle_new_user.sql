3c8cae971b5d125e1460accd8825f87382d877a6
RenzArcilla <renzarcilla09@gmail.com>
2026-04-25 18:05:59 +0800

    fix: correct trigger functions for user role sync

--- supabase/migrations/functions/handle_new_user.sql
+++ supabase/migrations/functions/handle_new_user.sql
@@ -17,6 +17,7 @@ $$ LANGUAGE plpgsql SECURITY DEFINER SET search_path = public;
 
 
 -- run the function for every insert in auth.users
+DROP TRIGGER IF EXISTS on_auth_user_created ON auth."users";
 CREATE TRIGGER on_auth_user_created
   AFTER INSERT ON auth.users
   FOR EACH ROW EXECUTE FUNCTION public.handle_new_user();
